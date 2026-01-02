31c78d9d92839be0592def38ec96c3d48601f78a
mrpawar221@gmail.com <nalwadesumati@gmail.com>
2026-01-02 17:12:50 +0530

    Post card using input decorators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCardComponent } from './post-card/post-card.component';
+import { PostDashboardComponent } from './post-dashboard/post-dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, PostCardComponent, PostDashboardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatTooltipModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/post-dashboard/post-dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, TrackByFunction } from '@angular/core';
+import { Ipost } from '../models/post';
+import { postsArr } from '../shared/const/post';
+
+@Component({
+  selector: 'app-post-dashboard',
+  templateUrl: './post-dashboard.component.html',
+  styleUrls: ['./post-dashboard.component.scss'],
+})
+export class PostDashboardComponent implements OnInit {
+  postsArr: Ipost[] = postsArr;
+  trackById!: TrackByFunction<Ipost>;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
